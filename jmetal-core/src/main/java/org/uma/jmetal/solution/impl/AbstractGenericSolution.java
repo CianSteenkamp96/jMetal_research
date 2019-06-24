327c3e26217cd31a0da7218bb905ffb17af014b8
ciansteenkamp96 <ciansteenkamp@gmail.com>
2019-06-24 11:38:48 +0200

    Changes

--- jmetal-core/src/main/java/org/uma/jmetal/solution/impl/AbstractGenericSolution.java
+++ jmetal-core/src/main/java/org/uma/jmetal/solution/impl/AbstractGenericSolution.java
@@ -75,6 +75,7 @@ public abstract class AbstractGenericSolution<T, P extends Problem<?>> implement
   public double getObjective_CDAS(int index) {  // CDAS changes f_i(x) => f_i`(x) = (r * sin(w_i - phi_i)) / sin(phi_i) where r = ||f(x)||, w_i = arccos(f_i(x) / ||f(x)||), and phi_i = S_i * pi.
     double r = Math.sqrt(Solution.dotProduct(objectives, objectives));
     double w_i = Math.acos((objectives[index] / r));
+    // S => {0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75}
     double s = 0.35; // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! User-specified s-val - update here !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     double phi_i = s * Math.PI;
     return (r * Math.sin(w_i - phi_i)) / Math.sin(phi_i); // f_i`(x)
